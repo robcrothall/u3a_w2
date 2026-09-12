26fab4143e6e1c059025eac837879a2f84e743a5
Rob Crothall <rob@crothall.co.za>
2026-09-13 00:12:23 +0200

    Fix .gitignore

--- app/templates/header.php
+++ app/templates/header.php
@@ -17,6 +17,10 @@
                 <a class="nav-link" href="/login.php">Login</a>
                 <a class="nav-link" href="/register.php">Register</a>
                 <?php else: ?>
+                <a class="nav-link" href="/change_password.php">Change Password</a>
+                <?php if (user_has_role((int) $_SESSION["id"], "admin")): ?>
+                <a class="nav-link" href="/admin/reset_password.php">Reset Member Password</a>
+                <?php endif; ?>
                 <a class="nav-link" href="/logout.php">Logout</a>
                 <?php endif; ?>
             </div>
